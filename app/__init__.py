6a2edcf04f40470dce475e4a12a460a7c055bd0c
Vanessa Sharine <vanessasharine@gmail.com>
2021-03-18 22:12:30 +0000

    Alteração do init para acessar o BD

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-from app import routes
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+from app import routes, models
+db = SQLAlchemy(app)

50c6caaf6eeabe5ce6b36d47f3551e9a2cf913dc
Vanessa Sharine <vanessasharine@gmail.com>
2021-03-18 19:44:20 +0000

    Adicionando o post

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,10 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
+    user = {'username': 'Vanessa'}
+    posts= [
+        {'author': {'username': 'Maria'},'body': 'Olá da Maria'},
+        {'author': {'username': 'Feulo'},'body': 'Olá'}
+    ]
 
-    return render_template("index.html", username= "Vanessa")
+    return render_template("index.html", user = user,posts = posts)
